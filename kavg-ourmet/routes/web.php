754e0f05e4f295f36efed452e5fd22f557a7b83b
Guilherme Gonçalves <guilhermegoncalves1314@gmail.com>
2023-10-25 22:17:02 -0300

    Subindo alteracao

--- kavg-ourmet/routes/web.php
+++ kavg-ourmet/routes/web.php
@@ -29,10 +29,22 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/home', function () {
+    return Inertia::render('Welcome');
+})->middleware(['auth', 'verified'])->name('home');
+
+Route::get('/rota-autenticada', 'NomeDoController@metodo')->middleware('auth');
+
+
+
+
+
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/produto', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/produto', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/produto', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    
 });
 
 require __DIR__.'/auth.php';
